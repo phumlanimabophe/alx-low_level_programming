fab269184cf0fc724f79b02ce8065225ed479e76
comspromo <be.standing.solutions@gmail.com>
2023-02-27 09:07:47 -0800

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square...
+ * @size: size of the square
+ * Return: void
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
