7204808f6730dc77ec71cdb033c14915717cc0d4
comspromo <be.standing.solutions@gmail.com>
2023-02-27 09:38:30 -0800

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,12 @@
 void puts2(char *s)
 {
 	int a;
-	for(a = 0; s[a] != '\0'; a++)
+
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		if(a % 2 ==0)
+		if (a % 2 == 0)
 		{
-		_putchar(s[a]);
+			_putchar(s[a]);
 		}
 	}
 	_putchar('\n');
