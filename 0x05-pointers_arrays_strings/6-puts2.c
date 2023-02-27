3ea6b98ec6b5d37eabfdeeae18a88cf8559adf71
comspromo <be.standing.solutions@gmail.com>
2023-02-27 09:36:47 -0800

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,16 @@
  * @str: The string containing characters.
  * Return: void
  */
-void puts2(char *str)
+
+void puts2(char *s)
 {
-	int i, len = 0;
-
-	while (str[i++])
-		len++;
-
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
-
+	int a;
+	for(a = 0; s[a] != '\0'; a++)
+	{
+		if(a % 2 ==0)
+		{
+		_putchar(s[a]);
+		}
+	}
 	_putchar('\n');
 }
