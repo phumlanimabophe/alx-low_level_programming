746f480fdaa4ea23ae201c8c68bc2f206759fe19
root <be.standing.solutions@gmail.com>
2023-03-01 14:27:32 -0800

    m

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -13,8 +12,4 @@ char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
 void print_number(int n);
-char* infinite_add(char* n1, char* n2, char* r, int size_r)
-void print_buffer(char *b, int size)
-
 #endif
-#endif /* MAIN_H */
