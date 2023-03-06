9933648a7fc830c3811c147cecbee1049d40ebbc
root <be.standing.solutions@gmail.com>
2023-03-06 12:14:15 -0800

    m

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-strcat.c
+ */
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer
+ * Return : Void function .
+ */
+void set_string(char **s, char *to)
+{
+	 *s = to;
+}
