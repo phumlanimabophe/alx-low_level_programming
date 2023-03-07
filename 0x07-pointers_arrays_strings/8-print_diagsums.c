d21a1033a1d94ea2ec085fc980ecff98d55509f4
root <be.standing.solutions@gmail.com>
2023-03-06 16:19:29 -0800

    m

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,30 +3,30 @@
  */
 
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - prints the sums of the diagonals of a matrix.
- * @a: pointer to the matrix.
- * @size: size of the matrix.
+ * main - Entry point
  *
- * Return: void.
+ * Return: Always 0.
  */
-void print_diagsums(int *a, int size)
+int main(void)
 {
-    int sum1 = 0, sum2 = 0;
-    int i;
+    int c3[3][3] = {
+        {0, 1, 5},
+        {10, 11, 12},
+        {1000, 101, 102},
+    };
+    int c5[5][5] = {
+        {0, 1, 5, 12124, 1234},
+        {10, 11, 12, 123521, 12512},
+        {1000, 101, 102, 12545, 214543435},
+        {100, 1012451, 11102, 12545, 214543435},
+        {10, 12401, 10452, 11542545, 1214543435},
+    };
 
-    for (i = 0; i < size; i++)
-    {
-        sum1 += *(a + i * size + i);
-        sum2 += *(a + i * size + (size - i - 1));
-    }
+    print_diagsums((int *)c3, 3);
+    print_diagsums((int *)c5, 5);
 
-    putchar(sum1 / 10 + '0');
-    putchar(sum1 % 10 + '0');
-    putchar(',');
-    putchar(' ');
-    putchar(sum2 / 10 + '0');
-    putchar(sum2 % 10 + '0');
-    putchar('\n');
+    return (0);
 }
