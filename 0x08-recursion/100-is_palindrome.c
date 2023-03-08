1d84e7546e495acfc49441cbf03ab77f281afbb3
root <be.standing.solutions@gmail.com>
2023-03-08 06:50:27 -0800

    0

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,51 @@
 /*
- * File: 0-memset.c
+ * File: 100
  */
 
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte .
- * @s: pass allocated memory - array of characters .
- * @b: pass char bytes .
- * @n: pass number of spaces to fill .
- * Return: pointer to memory area s
+ * _strlen_recursion - return length of string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
  */
-char *_memset(char *s, char b, unsigned int n)
+int _strlen_recursion(char *s)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-	return (s);
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
+
+/**
+ * palindrome_finder - finds palindromes
+ *
+ * @s: string to find palindrome of
+ * @l: last offset of string
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int palindrome_finder(char *s, int l)
+{
+	if (l < 1)
+		return (1);
+	if (*s == *(s + l))
+		return (palindrome_finder(s + 1, l - 2));
+	return (0);
+}
+
+/**
+ * is_palindrome - returns if string is palindrome or not
+ *
+ * @s: string to test
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (palindrome_finder(s, len - 1));
+}
+
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,36 +2,4 @@
  * File: 6
  */
 
-#include "main.h"
-
-/**
- * is_prime_number - Checks whether a given number is prime.
- * @n: The number to check.
- *
- * Return: 1 if n is prime, 0 otherwise.
- */
-int is_prime_number(int n)
-{
-    /* Base case: n is less than 2, so it's not prime */
-    if (n < 2)
-        return 0;
-
-    /* Base case: n is 2 or 3, so it's prime */
-    if (n == 2 || n == 3)
-        return 1;
-
-    /* Base case: n is even, so it's not prime */
-    if (n % 2 == 0)
-        return 0;
-
-    /* Recursive case: check odd divisors up to the square root of n */
-    int i;
-    for (i = 3; i * i <= n; i += 2)
-    {
-        if (n % i == 0)
-            return 0;
-    }
-
-    return 1;
-}
 
