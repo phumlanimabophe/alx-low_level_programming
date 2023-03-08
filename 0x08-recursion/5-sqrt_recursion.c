5daf7f7e832b7e1e38ea6e85bafeaf0ab3d8fb66
root <be.standing.solutions@gmail.com>
2023-03-08 06:25:48 -0800

    0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,19 +5,27 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte .
- * @s: pass allocated memory - array of characters .
- * @b: pass char bytes .
- * @n: pass number of spaces to fill .
- * Return: pointer to memory area s
+ * _sqrt_recursion - Finds the natural square root of a number using recursion.
+ * @n: The number to find the square root of.
+ *
+ * Return: The square root of n if it exists, otherwise -1.
  */
-char *_memset(char *s, char b, unsigned int n)
+int _sqrt_recursion(int n)
 {
-	unsigned int i;
+    /* Base case: n has a natural square root */
+    if (n == 0 || n == 1)
+        return n;
 
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-	return (s);
+    /* Base case: n does not have a natural square root */
+    if (n < 0)
+        return -1;
+
+    /* Recursive case: try a lower number */
+    int root = _sqrt_recursion(n / 4) * 2;
+    if (root * root <= n && (root + 1) * (root + 1) > n)
+        return root;
+    else if (root * root < n)
+        return root + 1;
+    else
+        return -1;
 }
