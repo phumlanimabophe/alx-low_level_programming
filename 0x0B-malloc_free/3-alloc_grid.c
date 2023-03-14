7637765735b74e4477aade0ceabc8d7a4d610b20
root <be.standing.solutions@gmail.com>
2023-03-14 05:38:44 -0700

    m

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,43 +7,45 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - Allocate a 2D grid of integers
- * @width: the number of columns
- * @height: the number of rows
+ * alloc_grid - Entry point
+ * @width: rows of matrix
+ * @height: columns of string
  *
- * Return: a pointer to the newly-allocated grid, or NULL on failure
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int row, col;
+    int **matrix;
+    int i;
+    int j;
+    int l;
+    int *p;
 
-	if (width <= 0 || height <= 0)
-		return NULL;
+    if (width <= 0 || height <= 0)
+        return NULL;
 
-	/* Allocate memory for the grid */
-	grid = malloc(height * sizeof(int *));
-	if (grid == NULL)
-		return NULL;
+    matrix = (int **) malloc(height * sizeof(int *));
+    if (matrix == NULL)
+        return (NULL);
 
-	/* Allocate memory for each row */
-	for (row = 0; row < height; row++) {
-		grid[row] = malloc(width * sizeof(int));
-		if (grid[row] == NULL) {
-			/* Free previously-allocated rows */
-			for (col = 0; col < row; col++)
-				free(grid[col]);
-			free(grid);
-			return NULL;
-		}
-	}
+    for (i = 0; i < height; i++)
+    {
+        *(matrix + i) = (int *) malloc(width * sizeof(int));
+        if (*(matrix + i) == NULL)
+        {
+            for (i = 0; i < height; i++)
+            {
+                p = matrix[i];
+                free(p);
+            }
+            free(matrix);
+            return NULL;
+        }
+    }
 
-	/* Initialize each element to zero */
-	for (row = 0; row < height; row++) {
-		for (col = 0; col < width; col++) {
-			grid[row][col] = 0;
-		}
-	}
+    for (l = 0; l < height; l++)
+        for (j = 0; j < width; j++)
+            matrix[l][j] = 0;
 
-	return grid;
+    return matrix;
 }
