225255d5cfca6b93f2ac39a2761dbb4da530f775
comspromo <93977501+comspromo@users.noreply.github.com>
2023-03-18 04:16:08 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,34 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
-/*
- * Concatenate two strings, up to n characters of the second string.
- * If s1 or s2 are NULL, treat them as empty strings.
- * Returns a new string that must be freed by the caller, or NULL on error.
+/**
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
  */
-char *concat_strings(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-    if (s1 == NULL) {
-        s1 = "";
-    }
+	void *n;
 
-    if (s2 == NULL) {
-        s2 = "";
-    }
+	n = malloc(b);
 
-    unsigned int len1 = strlen(s1);
-    unsigned int len2 = strlen(s2);
-
-    if (len2 > n) {
-        len2 = n;
-    }
-
-    char *result = malloc(len1 + len2 + 1);
-    if (result == NULL) {
-        return NULL;
-    }
-
-    strcpy(result, s1);
-    strncat(result, s2, len2);
-
-    return result;
+	if (n == NULL)
+		exit(98);
+	return (n);
 }
+
