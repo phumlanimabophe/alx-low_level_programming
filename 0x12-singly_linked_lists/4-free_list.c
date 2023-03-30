d4a571f9e2dfe984a7239a6525003c52741885d0
comspromo <93977501+comspromo@users.noreply.github.com>
2023-03-30 18:21:08 +0000

    gone

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
- * @head: pointer to head node of the list
- **/
+ * free_list - frees a linked list
+ * @head: head of linked list to free
+ *
+ * Return: void
+ */
 void free_list(list_t *head)
 {
-	list_t *current;
+    list_t *current;
 
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current->str);
-		free(current);
-	}
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current->str);
+        free(current);
+    }
 }
