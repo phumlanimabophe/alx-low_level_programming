2d8dc1440bd1097f8918b6416e3adaacf4a22c46
comspromo <93977501+comspromo@users.noreply.github.com>
2023-03-30 18:26:05 +0000

    gone

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,42 @@
 #include "lists.h"
 
 /**
- * add_node_end - Add a new node to the end of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the new node.
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the pointer of the head of the list
+ * @str: string to be added as content of the new node
  *
- * Return: If memory allocation fails or head is NULL, NULL.
- *         Otherwise, the address of the new element.
+ * Return: address of the new element or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *last = *head;
-    size_t len = 0;
+    list_t *new_node, *last_node;
+    unsigned int len_str;
 
-    while (str && str[len])
-        len++;
-
-    new = malloc(sizeof(list_t));
-    if (!new)
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
         return (NULL);
 
-    new->str  = strdup(str);
-    new->len  = len;
-    new->next = NULL;
+    len_str = 0;
+    while (str[len_str])
+        len_str++;
 
-    if (!*head)
-        *head = new;
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    new_node->len = len_str;
+    new_node->next = NULL;
 
+    if (*head == NULL)
+        *head = new_node;
     else
     {
-        while (last->next)
-            last = last->next;
-
-        last->next = new;
+        last_node = *head;
+        while (last_node->next)
+            last_node = last_node->next;
+        last_node->next = new_node;
     }
-
-    return (new);
+    return (new_node);
 }
