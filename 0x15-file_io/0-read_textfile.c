26d746859f9927a88add3f6a98e100a5df0bc6da
comspromo <93977501+comspromo@users.noreply.github.com>
2023-04-11 21:10:12 +0000

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * read_textfile - read text ...
- * @letters:number of letters to read size_t
- * @filename: name of file char
- * 
- * Return: read the file display it or error 0
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd = open(filename, O_RDONLY);
