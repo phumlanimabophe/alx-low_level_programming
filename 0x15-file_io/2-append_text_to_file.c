1c0b356bc59a0a3212344a3e332d136658806526
comspromo <93977501+comspromo@users.noreply.github.com>
2023-04-11 21:19:51 +0000

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,21 +9,27 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    if (!filename)
-        return -1;
+	int i = 0, file;
 
-    int fd = open(filename, O_APPEND | O_WRONLY);
-    if (fd == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    int len = 0;
-    if (text_content) {
-        while (text_content[len])
-            len++;
-        if (write(fd, text_content, len) == -1)
-            return -1;
+	if (text_content == NULL)
+    {
+        text_content = "";
     }
+		
 
-    close(fd);
-    return 1;
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
